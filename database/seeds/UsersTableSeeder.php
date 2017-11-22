ccf2b06ea7dfcfe3eaeef64279f6a571324160d4
keycoolkui <37068701@qq.com>
2017-11-22 20:50:20 +0800

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -45,6 +45,13 @@ class UsersTableSeeder extends Seeder
         $user->name = 'keycoolkui';
         $user->email = 'man@cstrong.cn';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+        $user->save();
+
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
         $user->save();
     }
 }

eda7b7d61eca2bc4e7be25836113b59bab212707
keycoolkui <37068701@qq.com>
2017-11-23 12:58:02 +0800

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,10 +6,11 @@ use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, User $user)
+    public function show(Category $category, Request $request, User $user, Link $link)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = Topic::withOrder($request->order)
@@ -17,6 +18,7 @@ class CategoriesController extends Controller
                         ->paginate(20);
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }

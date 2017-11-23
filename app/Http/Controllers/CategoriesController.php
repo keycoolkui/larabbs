953246b495ef0bde14a1a80e4ba04a6bd8f30410
keycoolkui <37068701@qq.com>
2017-11-23 12:32:49 +0800

    Sidebar active users

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request)
+    public function show(Category $category, Request $request, User $user)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = Topic::withOrder($request->order)
                         ->where('category_id', $category->id)
                         ->paginate(20);
-
-        return view('topics.index', compact('topics', 'category'));
+        // 活跃用户列表
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
